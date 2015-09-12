fc9e0977766d808aa1063109d91516f8648881c7
daveclifton <dave@clifton.org.uk>
2015-09-12 23:48:25 +0100

    postgres migration

--- audioguide/audioguide/settings.py
+++ audioguide/audioguide/settings.py
@@ -76,10 +76,30 @@ WSGI_APPLICATION = 'audioguide.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'default': {
+            # "postgresql://postgres:postgres@localhost:5432/audioguide"
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'audioguide',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        },
+
+        'default-dev': {
+            # "postgresql://postgres:postgres@localhost:5432/audioguide"
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'dcmqgo2erop023',
+            'USER': 'mwtcxzxsydvwrv',
+            'PASSWORD': 'fvXuVmRVwfueRXtOnoSmXGBeJr',
+            'HOST': 'ec2-54-225-134-223.compute-1.amazonaws.com',
+            'PORT': '5432',
+        },
+
+        'sqlite-default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
 }
 
 
